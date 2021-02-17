d478446113921e9b8b01bdca61293dfa14d30ac0
rohmat97 <dasukirohmat@gmail.com>
2021-02-17 18:53:17 +0700

    first init

---
+++ ios/Pods/Target Support Files/Pods-AturPOS-AturPOSTests/Pods-AturPOS-AturPOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AturPOS_AturPOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_AturPOS_AturPOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-AturPOS-tvOSTests/Pods-AturPOS-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AturPOS_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_AturPOS_tvOSTests
+@end
